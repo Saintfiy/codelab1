c0084131949ff9c4be07ab2cc001a37a28f23e62
Saintfiy <159164760+Saintfiy@users.noreply.github.com>
2024-03-25 04:11:13 +0700

    Add files via upload

---
+++ Aplikasi.java
@@ -0,0 +1,154 @@
+import java.util.Scanner;
+
+public class Aplikasi {
+    public static void main(String[] args) throws Exception {
+        Tabung tabung = new Tabung(" tabung");
+        Kubus kubus = new Kubus(" kubus");
+        Balok balok = new Balok(" balok");
+
+        balok.inputNilai();
+        balok.luasPermukaan();
+        balok.volume();
+
+        kubus.inputNilai();
+        kubus.luasPermukaan();
+        kubus.volume();
+
+        tabung.inputNilai();
+        tabung.luasPermukaan();
+        tabung.volume();
+    }
+}
+
+
+class Balok extends BangunRuang{
+    Scanner scanner = new Scanner(System.in);
+    private double panjang;
+    private double lebar;
+    private double tinggi;
+
+    public Balok(String namee) {
+        super(namee);
+    }
+
+    public void inputNilai(){
+        super.luasPermukaan();
+        super.inputNilai();
+        System.out.print("Input panjang: ");
+        panjang = scanner.nextDouble();
+        System.out.print("Input lebar: ");
+        lebar = scanner.nextDouble();
+        System.out.print("Input tinggi: ");
+        tinggi = scanner.nextDouble();
+    }
+
+    @Override
+    public void luasPermukaan(){
+        double hasil = 2 * ((panjang*lebar) + (panjang*tinggi) + (lebar*tinggi));
+
+        System.out.println("Hasil luas permukaan: " + hasil);
+    }
+
+    @Override
+    public void volume(){
+        double hasil = panjang * lebar * tinggi;
+        super.volume();
+        System.out.println("Hasil volume: " + hasil);
+    }
+}
+
+
+class BangunRuang {
+    private String name;
+
+    BangunRuang(String namee){
+        this.name = namee;
+    }
+
+    public void inputNilai(){
+        System.out.println("Silahkan Input Nilai ");
+    }
+
+    public void luasPermukaan(){
+        System.out.println("\nMenghitung luas permukaan bangun" + name);
+    }
+
+    public void volume(){
+        System.out.println("Menghitung volume bangun" + name);
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
+
+
+class Kubus extends BangunRuang {
+    Scanner scanner = new Scanner(System.in);
+    private double sisi;
+
+    public Kubus(String name) {
+        super(name);
+    }
+
+    @Override
+    public void inputNilai(){
+        super.luasPermukaan();
+        super.inputNilai();
+        System.out.print("Input Panjang Sisi: ");
+        sisi = scanner.nextDouble();
+    }
+
+    @Override
+    public void luasPermukaan(){
+        double hasil = 6 * Math.pow(sisi, 2);
+        System.out.println("Hasil Luas Permukaan: " + hasil);
+    }
+
+    @Override
+    public void volume(){
+        double hasil = Math.pow(sisi, 3);
+        super.volume();
+        System.out.println("Hasil Volume: " + hasil);
+    }
+}
+
+
+class Tabung extends BangunRuang{
+    Scanner scanner = new Scanner(System.in);
+    private double tinggi;
+    private double jari_jari;
+
+    Tabung(String nameBangun) {
+        super(nameBangun);
+    }
+
+    @Override
+    public void inputNilai(){
+        super.luasPermukaan();
+        super.inputNilai();
+        System.out.print("Input Tinggi: ");
+        tinggi = scanner.nextDouble();
+        System.out.print("Input Panjang Jari-Jari: ");
+        jari_jari = scanner.nextDouble();
+    }
+
+    @Override
+    public void luasPermukaan(){
+        double hasil = 2 * Math.PI * jari_jari * (jari_jari + tinggi);
+        System.out.println("Hasil Luas Permukaan: " + hasil);
+    }
+
+
+    @Override
+    public void volume(){
+        double hasil = Math.PI * Math.pow(jari_jari, 2) * tinggi;
+        super.volume();
+        System.out.println("Hasil Volume: " + hasil);
+    }
+}
